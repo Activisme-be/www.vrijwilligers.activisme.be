49af2f7c91afbbf100e870a550730219fe981a8d
Tim Joosten <Tjoosten3@gmail.com>
2019-08-18 21:56:22 +0200

    Fix #47 Refactoring SecurityValidator.php

--- app/Http/Requests/Users/SecurityValidator.php
+++ app/Http/Requests/Users/SecurityValidator.php
@@ -11,16 +11,6 @@ use ActivismeBe\ValidationRules\Rules\MatchUserPassword;
 class SecurityValidator extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
