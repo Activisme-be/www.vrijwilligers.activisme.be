b63dab2a3f7806533898e5460393d1bb98fc2133
Tim Joosten <Tjoosten@users.noreply.github.com>
2019-09-08 03:08:06 +0200

    Develop next (#62)
    * fix #54 Active state on alerts menu
    * fix #51 Active state user settngs menu

--- resources/views/notifications/kiosk/_partials/sidenav.blade.php
+++ resources/views/notifications/kiosk/_partials/sidenav.blade.php
@@ -4,11 +4,11 @@
     </div>
 
     <div class="list-group list-group-flush">
-        <a href="{{ route('alerts.index') }}" class="list-group-item list-group-item-action">
+        <a href="{{ route('alerts.index') }}" class="{{ active('alerts.index', 'font-weight-bold') }} list-group-item list-group-item-action">
             <i class="fe fe-bell text-muted mr-2"></i> Notificatie verzenden
         </a>
 
-        <a href="{{ route('alerts.overview') }}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+        <a href="{{ route('alerts.overview') }}" class="{{ active('alerts.overview', 'font-weight-bold') }} list-group-item list-group-item-action d-flex justify-content-between align-items-center">
             <span class="float-left"><i class="fe fe-list text-muted mr-2"></i> Verzonden notificaties</span>
             <span class="badge badge-primary badge-pill">{{ $notifications_count }}</span>
         </a>
--- resources/views/users/components/settings-sidenav.blade.php
+++ resources/views/users/components/settings-sidenav.blade.php
@@ -4,18 +4,18 @@
     </div>
 
     <div class="list-group list-group-flush">
-        <a href="{{ route('account.settings') }}" class="list-group-item list-group-item-action">
+        <a href="{{ route('account.settings') }}" class="{{ active('account.settings', 'font-weight-bold') }} list-group-item list-group-item-action">
             <i class="text-secondary fe fe-info mr-1"></i> Account informatie
         </a>
 
-        <a href="{{ route('account.security') }}" class="list-group-item list-group-item-action">
+        <a href="{{ route('account.security') }}" class="{{ active('account.security', 'font-weight-bold') }} list-group-item list-group-item-action">
             <i class="text-secondary fe fe-shield mr-1"></i> Account beveiliging
         </a>
     </div>
 </div>
 
 <div class="list-group">
-    <a href="{{ route('users.destroy', $currentUser) }}" class="list-group-item list-group-item-danger list-group-item-action">
+    <a href="{{ route('users.destroy', $currentUser) }}" class="{{ active('users.destroy') }} list-group-item list-group-item-danger list-group-item-action">
         <i class="fe fe-user-x mr-1"></i> Verwijder account
     </a>
 </div>
