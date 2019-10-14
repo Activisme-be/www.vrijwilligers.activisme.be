541adc89db67e91e0998a11a381a06fe235d5f30
Tim Joosten <Tjoosten@users.noreply.github.com>
2019-10-14 18:11:15 +0200

    Set description to docblocks (#84)

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -5,19 +5,17 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\VerifiesEmails;
 
+/**
+ * Class VerificationController
+ *
+ * This controller is responsible for handling email verification for any
+ * user that recently registered with the application. EMails may also
+ * be re-sent if the user didn't receive the original email message.
+ *
+ * @package App\Http\Controllers\Auth
+ */
 class VerificationController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Email Verification Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling email verification for any
-    | user that recently registered with the application. Emails may also
-    | be re-sent if the user didn't receive the original email message.
-    |
-    */
-
     use VerifiesEmails;
 
     /**
