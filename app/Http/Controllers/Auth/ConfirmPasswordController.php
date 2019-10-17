e4a724ff68c3ce7d841cc7918f067b468438ca7a
Tim Joosten <Tjoosten@users.noreply.github.com>
2019-10-17 04:00:10 +0200

    Fix #88 Laravel sync (#89)
    * Add new password rule language line
    * Remove middleware from paassword reset
    * Implement password confirmation.
    * Formatting
    * Fix required version of the framework within 'composer.json'
    * Add xml schema to phpunit

---
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ConfirmsPasswords;
+
+/**
+ * Class ConfirmPasswordController
+ *
+ * This controller is responsible for handling password confirmations and
+ * uses a simple trait to include the behavior. You're free to explore
+ * this trait and override any functions that require customization.
+ *
+ * @package App\Http\Controllers\Auth
+ */
+class ConfirmPasswordController extends Controller
+{
+    use ConfirmsPasswords;
+
+    /**
+     * Where to redirect users when the intended url fails.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+}
--- config/auth.php
+++ config/auth.php
@@ -99,4 +99,16 @@ return [
         ],
     ],
 
+    /*
+     |--------------------------------------------------------------------------
+     | Password confirmation Timeout
+     |--------------------------------------------------------------------------
+     |
+     | Here you may define the amount of seconds before a password confirmation
+     | times out and the user is prompted to re-enter their password via the
+     | confirmation screen. By default, the timeout lasts for three hours.
+     */
+
+    'password_timeout' => 10800,
+
 ];
