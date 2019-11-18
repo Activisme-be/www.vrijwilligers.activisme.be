184cce05b403fb7dfa6f3bc176286421c25ebcee
Tjoosten <Topairy@gmail.com>
2019-11-18 16:58:26 +0100

    Fix wrong facade

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use App\Models\User;
 use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Password;
+use Laravel\Passport\Passport;
 
 /**
  * Class AuthServiceProvider
@@ -29,6 +29,6 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->registerPolicies();
-        Password::routes();
+        Passport::routes();
     }
 }
